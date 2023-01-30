f2b32abbdb181c261bd29196327c935b0ccbf337
syedmuhammadfarhan <114781442+syedmuhammadfarhan@users.noreply.github.com>
2023-01-31 00:07:03 +0500

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body >
+        {/* <center > */}
+        {children}
+        {/* </center> */}
+      </body>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,91 +1,69 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
+import './add.css';
+import { useState } from "react";
+
+
 
 export default function Home() {
+
+  const [input, setinput] = useState("")
+  const [add, setadd] = useState([""])
+
+
+  const addinput = () => {
+    if (input === add.find((f) => f === input)) {
+      return false;
+    } else {
+      const listArr = [...add, input]
+      setadd(listArr)
+      setinput('')
+
+    }
+  };
+
+  const deleteinput = (e: any) => {
+    const listArr = add.filter((f) => {
+      if (f === e) return false;
+      return true;
+    });
+    setadd(listArr);
+  };
+
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
-      </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <h1>ToDo App</h1>
+      <input type="text" value={input} placeholder="Add input" onChange={(e) => setinput(e.target.value)} />
+      <button className="add" onClick={addinput}>Add</button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+
+      <ul>
+        {add.map((e: any) => {
+          if (e) {
+            return (
+              <li style={{
+                fontStyle: "oblique",
+                fontWeight: "bold",
+                listStyle: "none",
+              }}
+                key={e}>
+                <input type="checkbox" style={{ margin: "10px" }} />
+                {e}
+
+                <button style={{ backgroundColor: "coral", margin: "10px", color: "black", fontFamily: "fantasy", }} onClick={() => { deleteinput(e); }}>Delete</button>
+              </li>
+
+            )
+          }
+        })
+        }
+      </ul>
+    </>
   )
 }
