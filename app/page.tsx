5e368c819485a9e97b60a4286e8b8e944baf4498
syedmuhammadfarhan <farhanpiaic@gmail.com>
2023-01-31 14:41:05 +0500

    button border round

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,19 @@
 "use client";
 
-import './add.css';
+import "./add.css";
 import { useState } from "react";
 
-
-
 export default function Home() {
-
-  const [input, setinput] = useState("")
-  const [add, setadd] = useState([""])
-
+  const [input, setinput] = useState("");
+  const [add, setadd] = useState([""]);
 
   const addinput = () => {
     if (input === add.find((f) => f === input)) {
       return false;
     } else {
-      const listArr = [...add, input]
-      setadd(listArr)
-      setinput('')
-
+      const listArr = [...add, input];
+      setadd(listArr);
+      setinput("");
     }
   };
 
@@ -30,40 +25,56 @@ export default function Home() {
     setadd(listArr);
   };
 
-
-
   return (
     <>
-
-
       <h1>ToDo App</h1>
-      <input type="text" value={input} placeholder="Add input" onChange={(e) => setinput(e.target.value)} />
-      <button className="add" onClick={addinput}>Add</button>
-
-
-
+      <input
+        type="text"
+        value={input}
+        placeholder="Add input"
+        onChange={(e) => setinput(e.target.value)}
+      />
+      <button className="add" onClick={addinput}>
+        Add
+      </button>
 
       <ul>
         {add.map((e: any) => {
           if (e) {
             return (
-              <li style={{
-                fontStyle: "oblique",
-                fontWeight: "bold",
-                listStyle: "none",
-              }}
-                key={e}>
+              <li
+                style={{
+                  fontStyle: "oblique",
+                  fontWeight: "bold",
+                  listStyle: "none",
+                  
+                  listStylePosition: "inside",
+                  listStyleType: "none",
+                }}
+                key={e}
+              >
                 <input type="checkbox" style={{ margin: "10px" }} />
                 {e}
 
-                <button style={{ backgroundColor: "coral", margin: "10px", color: "black", fontFamily: "fantasy", }} onClick={() => { deleteinput(e); }}>Delete</button>
+                <button
+                  style={{
+                    backgroundColor: "coral",
+                    margin: "10px",
+                    color: "black",
+                    fontFamily: "fantasy",
+                    borderRadius: "6px",
+                  }}
+                  onClick={() => {
+                    deleteinput(e);
+                  }}
+                >
+                  Delete
+                </button>
               </li>
-
-            )
+            );
           }
-        })
-        }
+        })}
       </ul>
     </>
-  )
+  );
 }
